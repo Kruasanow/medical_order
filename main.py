3215240be868daac271aa8ff16d2fed3cd79aff8
Kruasanow <rusanow.mitya@bk.ru>
2023-05-14 00:40:21 +0300

    form

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 
-from flask import Flask, render_template, url_for, request, session
+from flask import Flask, render_template, url_for, request, session, redirect
 
 app = Flask(__name__)    
+app.secret_key = 'rabotaetspecnaz'
 
-@app.route('/', methods = ['get','post'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    print(url_for('index'))
-    
-    return render_template('index.html')
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        session['username'] = username
+
+        if username == 'dima' and password == 'pass':
+            return redirect(url_for('secpage'), code=301)
+        else:
+            error = 'Invalid Credentials. Please try again.'
+    return render_template('index.html', error=error)
+
+@app.route('/secpage', methods=['GET', 'POST'])
+def secpage():
+    username = session['username']
+    return render_template('secpage.html', username = username)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=False)
